0eec2dc66b5b6f711e32ea0b6c1fe488cdfe0323
Adam <ashaw315@gmail.com>
2021-10-31 20:29:24 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution in this file!
+let employee = {
+    sam: "11 broadway"
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return Object.assign({}, employee, { [key]: value});
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key]=value;
+return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = Object.assign({}, employee, [key])
+    delete newEmployee[key];
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
